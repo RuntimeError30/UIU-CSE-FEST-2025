278e5aaa460db385a9db697687981b2bc51d0a80
RuntimeError30 <mdkfahim@gmail.com>
2024-11-23 12:22:29 -0500

    updated

--- uiucsefestcore/settings.py
+++ uiucsefestcore/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-&^8bhg0zl9oit1n13*jh&v7zj0h1$9oj3+@w8@xgbk7n)_#2md"
 
 
 
